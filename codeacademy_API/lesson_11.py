6f307b3e88ae98c61155170b35d7eed89afdbd67
ShisuiUchiha <santhukota1996@gmail.com>
2017-06-01 17:52:33 +0530

    Delete lesson_11.py

--- codeacademy_API/lesson_11.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# Make a GET request here and assign the result to kittens:
-kittens=requests.get('http://placekitten.com/')
-
-
-print kittens.headers	
-print kittens.text[559:1000]
